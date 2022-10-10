7c7be2bbee4e7cd637586446051e62c360962a07
Nayan Bhut <nayanbhut03@gmail.com>
2022-10-10 00:40:57 -0400

    refactor: ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,31 +1,26 @@
-import { CommandSlashCommand } from '@utils';
+import { CommandSlashCommand, Log } from '@utils';
 import { Colors, EmbedBuilder } from 'discord.js';
 
 export const ping = new CommandSlashCommand({
   name: 'ping',
   data: { name: 'ping', description: 'Displays your ping' },
   async execute(interaction) {
-    const pingEmbed = new EmbedBuilder({
+    const deferedMessage = await interaction
+      .deferReply({ fetchReply: true })
+      .catch(Log.error);
+
+    if (!deferedMessage) return;
+
+    const pingMeasuredEmbed = new EmbedBuilder({
       title: 'Ping',
-      description:
-        'Shows the latency of between sending a command and recieving a command response.',
       color: Colors.Blurple,
+      description: `Your ping is ⏳ ${
+        deferedMessage.createdTimestamp - interaction.createdTimestamp
+      } ms!`,
     });
 
-    try {
-      const responseMsg = await interaction.deferReply({ fetchReply: true });
-      pingEmbed.setDescription(
-        `Your ping is ⏳ ${
-          responseMsg.createdTimestamp - interaction.createdTimestamp
-        } ms!`,
-      );
-    } catch (err) {
-      pingEmbed
-        .setTitle('Error:')
-        .setColor(Colors.Red)
-        .setDescription('Something went wrong please try again!');
-    }
-
-    return interaction.editReply({ embeds: [pingEmbed] });
+    return interaction
+      .editReply({ embeds: [pingMeasuredEmbed] })
+      .catch(Log.error);
   },
 });
